12fc3d1a252b09ffa9dba3808e7ee3743925227c
Pavel Sušický <me@frontendista.cz>
2024-11-19 12:41:53 +0100

    chore(ESLint): add stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default [
 	...eslintPluginAstro.configs.recommended,
 	{
-		rules: {}
+		plugins: {
+			"@stylistic": stylistic
+		},
+		rules: {
+			"@stylistic/indent": ["error", "tab"],
+			"@stylistic/indent-binary-ops": ["error", "tab"],
+			"@stylistic/semi": ["error", "always"],
+			"@stylistic/quotes": ["error", "double"],
+		}
 	}
 ];
